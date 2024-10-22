3680048cbb5d4b8c214bf74f5253df7d1d19a7c2
monika638 <monika638@gmail.com>
2024-10-22 12:22:11 +1100

    a new component added with the name message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,42 @@
 @includeIf('common.header')
-@includeIf('common.banner' ,['page' => 'This si my Home page'])
+@includeIf('common.banner' ,['page' => 'This is my Home page'])
+<style>
+    .success
+    {
+        background-color:lightgreen;
+        color:green;
+        padding:10px;
+        border-radius:10px;
+        margin-top:10px;
+        text-align: center;
+        display:inline-flex;
+    }
+    .error
+    {
+        background-color:lightcoral;
+        color:red;
+        padding:10px;
+        border-radius:10px;
+        margin-top:10px;
+        text-align: center;
+        display:inline-flex;
+    }
+    .warning
+    {
+        background-color:lightsalmon;
+        color:yellow;
+        padding:10px;
+        border-radius:10px;
+        margin-top:10px;
+        text-align: center;
+        display:inline-flex;
+    }
+</style>    
+<div>
+    <x-message-banner  msg="User Logged In successfully" class="success" />
+    <x-message-banner  msg="Password doesnot work" class="error" />
+    <x-message-banner  msg="something went wrong" class="warning" />
+</div>
 <h1>Hello I am here Laravel</h1>
 <a href="/about">About</a>
 <a href="/contact">Contact</a>
